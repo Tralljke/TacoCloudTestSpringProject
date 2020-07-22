4d047498f26dd8c93517b385d5063a7bf2b773aa
Petr Kacman <petr_bel96@icloud.com>
2020-07-22 19:16:37 +0700

    end

--- src/main/java/tacos/OrderController.java
+++ src/main/java/tacos/OrderController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
@@ -17,6 +18,15 @@ public class OrderController {
     public String orderForm(Model model) {
         model.addAttribute("order", new Order());
         return "orderForm";
+    }
+    @PostMapping
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        log.info("Order submitted: " + order);
+        return "redirect:/";
     }
 }
 
