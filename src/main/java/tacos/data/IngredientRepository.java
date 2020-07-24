9153476c90f5263d9cb10b8cc6b88fae50b86c10
Petr Kacman <petr_bel96@icloud.com>
2020-07-24 22:51:42 +0700

    end

--- src/main/java/tacos/data/IngredientRepository.java
+++ src/main/java/tacos/data/IngredientRepository.java
@@ -1,7 +1,13 @@
 package tacos.data;
+
 import tacos.Ingredient;
+
 public interface IngredientRepository {
+
     Iterable<Ingredient> findAll();
-    Ingredient findOne(String id);
+
+    Ingredient findById(String id);
+
     Ingredient save(Ingredient ingredient);
+
 }
